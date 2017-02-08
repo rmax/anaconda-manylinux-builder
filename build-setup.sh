6509d20f6a1c4fd7145117ec194cb45eec690385
Rolando Espinoza <rndmax84@gmail.com>
2017-02-08 02:39:48 -0400

    WIP

--- build-setup.sh
+++ build-setup.sh
@@ -1,7 +1,11 @@
 #
 # Add additional setup steps here.
 #
-#echo "multilib_policy=best" >> /etc/yum.conf
-#yum install -y libffi-devel
-#
-#export CFLAGS="${CFLAGS} $(pkg-config --cflags-only-I libffi)"
+echo "multilib_policy=best" >> /etc/yum.conf
+
+yum install -y openssl101e-devel openssl101e-static libffi-devel
+
+export CFLAGS="${CFLAGS} $(pkg-config --cflags-only-I openssl101e)"
+export LDFLAGS="${LDFLAGS} $(pkg-config --libs-only-L openssl101e)"
+
+yum install -y openblas-devel openblas-static
