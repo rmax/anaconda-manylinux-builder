13f9c094c82641d2b89dbec458205040b576ffed
Rolando Espinoza <rndmax84@gmail.com>
2017-02-08 04:40:21 -0400

    Prefer x86_64 yum packages only.

--- build-setup.sh
+++ build-setup.sh
@@ -1,6 +1,7 @@
 #
 # Add additional setup steps here.
 #
+#echo "multilib_policy=best" >> /etc/yum.conf
 #yum install -y openssl101e-devel libffi-devel
 #
 #export CFLAGS="${CFLAGS} $(pkg-config --cflags-only-I openssl101e)"
